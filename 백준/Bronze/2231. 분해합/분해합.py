4736dd90332259bf1305f22327bc3a58cd21b991
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-08-12 20:39:03 +0900

    [Bronze II] Title: 분해합, Time: 296 ms, Memory: 115056 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -3,8 +3,8 @@ import sys
 # 보통 브루트 포스의 문제의 범위는 1,000,000으로 주어지는 경우가 많다.
 N = int(sys.stdin.readline().strip())
 for i in range(1, N+1):
-    arr = list(map(int, str(i)))  # str 변환하여 숫자를 하나씩 분리하여 int 리스트에 넣음
-    result = i + sum(arr)  # arr 값들을 더하고 i를 더함
+    arr = sum(map(int, str(i)))  # str 변환하여 숫자를 하나씩 분리하여 int 리스트에 넣음
+    result = i + arr  # arr 값들을 더하고 i를 더함
     if result == N:  # 결과가 N과 같으면 출력 후 멈춤
         print(i)
         break
