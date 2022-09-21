b05706292eeb25ed770b64fef3b5303a296f90ea
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-09-21 16:05:16 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,10 +1,10 @@
 import sys
 
-t = int(input())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(t)]
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(int(input()))]
 for i in range(len(arr)):
     cnt = 1
     for j in range(len(arr)):
         if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
             cnt += 1
     print(cnt, end=' ')
+    
