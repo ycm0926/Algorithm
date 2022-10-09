5400999d120aab4b5bd50e2ffa2a7ac016a4b18b
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-10-09 17:20:03 +0900

    [Silver IV] Title: 수 찾기, Time: 196 ms, Memory: 49760 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+n_arr = set(list(map(int, input().split())))
+m = int(input())
+m_arr = list(map(int, input().split()))
+
+for i in m_arr:
+    if i in n_arr:
+        print(1)
+    else:
+        print(0)
