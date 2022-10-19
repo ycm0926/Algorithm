2c742e1a96b3881b6a29eeeff9a62a50fc7c7850
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-10-19 15:12:58 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 324 ms, Memory: 116668 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,25 @@
+while 1:
+    words = input()
+    stack = []
+
+    if words == '.':
+        break
+
+    for i in words:
+        if i == '[' or i == '(':  # [ or [ 입력시 스택 추가
+            stack.append(i)
+        elif i == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(i)
+        elif i == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+    if not stack:
+        print('yes')
+    else:
+        print('no')
+
