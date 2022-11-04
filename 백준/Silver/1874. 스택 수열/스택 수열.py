2c464e081df38f165beff702637f6ee92c443ab6
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-04 17:01:22 +0900

    [Silver II] Title: 스택 수열, Time: 1564 ms, Memory: 131284 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -3,25 +3,25 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
-ans_number = []
-ans = []
+ans1 = []
+ans2 = []
 num_in = [int(input()) for _ in range(n)]
 num_li = [i for i in range(1, n+1)]
 num_li.sort(reverse=True)
 
 i = 0
 while i != n:
-    try:
-        if num_in[i] not in ans_number:
-            ans_number.append(num_li.pop())
-            ans.append('+')
-        if num_in[i] in ans_number:
-            ans_number.pop()
-            ans.append('-')
-            i += 1
-    except:
-        print('NO')
-        exit()
+    if num_in[i] not in ans1:
+        ans1.append(num_li.pop())
+        ans2.append('+')
+    if num_in[i] in ans1:
+        if num_in[i] == ans1[-1]:
+            ans1.pop()
+            ans2.append('-')
+        else:
+            print('NO')
+            exit()
+        i += 1
 
-for j in ans:
+for j in ans2:
     print(j)
