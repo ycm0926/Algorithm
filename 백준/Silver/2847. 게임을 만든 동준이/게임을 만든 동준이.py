33c50376f790c5dc1fe3f1e3ce81e8568ef2246c
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-09 15:15:50 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -0,0 +1,13 @@
+n = int(input())
+level = [int(input()) for _ in range(n)]
+level.reverse()
+ans = 0
+
+for i in range(n - 1):
+    while 1:
+        if level[i] > level[i+1]:
+            break
+        else:
+            level[i+1] -= 1
+            ans +=1
+print(ans)
