ac0d76071abb9ceb1b6d058b9b5fa189251d22eb
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-09 21:21:10 +0900

    [Silver V] Title: 수들의 합, Time: 116 ms, Memory: 114408 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -9,18 +9,13 @@
 6 1,2,3 i=5
 11 1,2,3,4,5 i=5
 '''
-n = int(input())
-arr = []
+S = int(input())
 total = 0
-i = 1
+i = 0
+
 while 1:
-    total += i
-    if total == n:
-        print(len(arr)+1)
-        break
-    elif total > n:
-        total -= 2*i-1
-        arr.pop()
-        continue
-    arr.append(i)
     i += 1
+    total += i
+    if total + i+1 > S:
+        print(i)
+        break
