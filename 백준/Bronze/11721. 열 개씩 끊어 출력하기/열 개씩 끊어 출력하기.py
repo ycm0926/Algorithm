3190c36bfe4e6227107523c07f370dd2cf22657a
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-13 19:43:01 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
@@ -1,2 +1,10 @@
+# 끝점을 기준
 n = input()
 for i in range(len(n)):print(n[i]) if i % 10 == 9 else print(n[i], end='')
+'''
+# 시작점을 기준
+for i in range(len(n)):
+    if i % 10 == 0:
+        j = i + 10
+        print(n[i:j])
+'''
