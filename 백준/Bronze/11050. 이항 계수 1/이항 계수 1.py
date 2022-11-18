2dc7128ea8e055248e180a00e76eccbafeb396ab
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-18 18:31:11 +0900

    [Bronze I] Title: 이항 계수 1, Time: 72 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,5 +1,16 @@
 import math
+a, b = map(int, input().split())
+res_a = 1
+res_b = 1
+res_c = 1
+for i in range(1,a+1):
+    res_a *= i
+for j in range(1,b+1):
+    res_b *= j
 
-n, k = map(int, input().split())
+for k in range(1,a-b+1):
+    res_c *= k
+print(res_a//(res_b*res_c))
 
-print((math.factorial(n)//(math.factorial(k)*math.factorial(n-k))))
+# a, b = map(int, input().split())
+# print((math.factorial(a)//(math.factorial(b)*(math.factorial(n-k)))))
