2ba737b9a5cd3f06f6e8ec1fd93ec02fab067d4a
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-19 15:04:03 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 166 ms, Memory: 60,616 KB -BaekjoonHub

---
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/README.md
@@ -0,0 +1,11 @@
+# [D3] [S/W 문제해결 기본] 5일차 - Magnetic - 1220 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14hwZqABsCFAYD) 
+
+### 성능 요약
+
+메모리: 60,616 KB, 시간: 166 ms, 코드길이: 388 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -0,0 +1,14 @@
+for tc in range(1, 11):
+    input()
+    graph = [list(map(int, input().split())) for _ in range(100)]
+    res = 0
+    for i in range(100):
+        check = False
+        for j in range(100):
+            if graph[j][i] == 1:
+                check = True
+            if check == True and graph[j][i] == 2:
+                res += 1
+                check = False
+    print(f'#{tc} {res}')
+    
