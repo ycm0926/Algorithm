61aef32ec684fca9a1b5eb592890dd4c85f3a02e
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-21 02:15:30 +0900

    [Bronze I] Title: 팰린드롬수, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,11 +1,32 @@
 import sys
 
-while True:
-    arr = list(map(int, sys.stdin.readline().rstrip()))
-    arr2 = list(reversed(arr))
-    if sum(arr) == 0:
+input = sys.stdin.readline
+while 1:
+    lst = list(map(int,input().strip()))
+    lst2 = list(reversed(lst))
+    if sum(lst) == 0:
         break
-    elif arr == arr2:
+    elif lst == lst2:
         print('yes')
     else:
         print('no')
+'''
+import sys
+
+input = sys.stdin.readline
+while 1:
+    lst = list(map(int, input().strip()))
+    lst_l = len(lst)
+    cnt = 0
+    if lst[0] == 0 and lst_l == 1:
+        break
+    for i in range(lst_l):
+        if lst[i] == lst[-i-1]:
+            cnt += 1
+            if cnt == lst_l//2:
+                print('yes')
+                break
+        else:
+            print('no')
+            break
+'''
