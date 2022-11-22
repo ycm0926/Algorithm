ac81615fb66a096d7752a15e9e27aa20a6195ba2
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-22 18:02:52 +0900

    [level 1] Title: 평균 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    return (sum(arr)/len(arr)) # 리스트 값들을 더한 후 길이 만큼 나눠준다
+
+'''
+# 다른사람 풀이
+
+from functools import reduce
+def solution(list):
+    # reduce 초기값이 없다면 lambda를 통해 list 에서 두 개의 값을 가져와 더한 후 결과를 x에 저장하고 다시 list에서 y값을 가져와 x에 계속 더해 준다.
+    return(reduce(lambda x, y : x + y, list) / len(list)) 
+
+TIL/회고
+- 문제는 쉽게 풀렸으며, reduce 함수의 사용법을 처음 알게됐다.
+'''
