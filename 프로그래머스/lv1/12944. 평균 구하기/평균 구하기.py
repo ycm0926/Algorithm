e5ee6f3fe1bec5ca788c20d8b5869a53b9358142
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-22 19:02:03 +0900

    [level 1] Title: 평균 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
@@ -5,7 +5,7 @@ def solution(arr):
 # 다른사람 풀이
 
 from functools import reduce
-def solution(list):
+def solution(n):
     # reduce 초기값이 없다면 lambda를 통해 list 에서 두 개의 값을 가져와 더한 후 결과를 x에 저장하고 다시 list에서 y값을 가져와 x에 계속 더해 준다.
     return(reduce(lambda x, y : x + y, list) / len(list)) 
 
