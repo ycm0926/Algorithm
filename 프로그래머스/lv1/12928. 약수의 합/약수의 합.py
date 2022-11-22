ceef01150096e6f25cfd5cf73d75ce780e6222e1
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-22 19:01:40 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -8,11 +8,8 @@ def solution(n):
 '''
 # 다른사람 풀이
 
-def sumDivisor(num):
+def solution(n):
     return sum([i for i in range(1,num+1) if num%i==0])
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(sumDivisor(12))
 
 TIL/회고
 - 1레벨이라 기본적인 쉬운문제
