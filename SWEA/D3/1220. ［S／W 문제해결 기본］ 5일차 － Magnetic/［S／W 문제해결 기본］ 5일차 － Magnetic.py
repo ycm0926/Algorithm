f26fc2208994a0115515ddbd7ba616dd55a54efb
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-23 12:26:09 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 162 ms, Memory: 60,608 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -7,7 +7,7 @@ for tc in range(1, 11):
         for j in range(100):
             if graph[j][i] == 1:
                 check = True
-            if check == True and graph[j][i] == 2:
+            if check == True and graph[j][i] == 2: # 1을 발견하고 2를 발견 시
                 res += 1
                 check = False
     print(f'#{tc} {res}')
