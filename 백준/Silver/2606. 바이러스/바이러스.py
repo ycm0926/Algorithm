d537a11f98e2182bb1e808866a0c633e64644c55
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-27 01:29:04 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,20 @@
+N = int(input())
+M = int(input())
+graph = [[] for i in range(N+1)]
+visited = [False] * (N+1)
+cnt = 0
+
 def dfs(v):
-    global ans
+    global cnt
     visited[v] = True
-    for i in g[v]:
+    for i in graph[v]:
         if not visited[i]:
+            cnt += 1
             dfs(i)
 
-n = int(input())
-m = int(input())
-
-g = [[] for i in range(n+1)]
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    g[a].append(b)
-    g[b].append(a)
-
-
-visited = [False] * (n+1)
+for _ in range(M):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 dfs(1)
-visited[1] = False
-print(visited.count(True))
+print(cnt)
