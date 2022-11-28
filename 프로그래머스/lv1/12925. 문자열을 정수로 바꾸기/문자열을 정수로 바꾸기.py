fba81478f6e1bbec572026baa85b4fa522857c8f
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-28 16:21:18 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -0,0 +1,25 @@
+# 나의 풀이
+def solution(s):
+    li = []
+    for i in s:
+        for j in i:
+            if j == '"':
+                continue
+            elif j == '-':
+                li.append(j)
+            else:
+                li.append(j)
+
+    li = ''.join(li)
+    return int(li)
+# 다른사람 풀이
+def solution(str):
+    a = int(str)
+    return a
+
+'''
+TIL/회고
+
+- int리스트는 join 불가하다.
+- 다른사람 풀이 파이썬은 정수형으로 바꿀 때 자동으로 -,+는 변환을 해준다.
+'''
