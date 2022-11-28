b83e1f02d685ddf0c70da0d16fce3a96a9773860
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-29 04:27:27 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 264 ms, Memory: 49788 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,9 +1,16 @@
+# 나의 풀이 (python3 276ms)
 import sys
 input = sys.stdin.readline
 N, M = map(int,input().split())
-res = {}
+res = {}  # 딕셔너리 선언
 for _ in range(N):
-    a,b = input().strip().split()
+    a,b = input().strip().split()  # a,b로 키,값 받음
     res[a] = b
 for i in range(M):
-    print(res[input().strip()])
+    print(res[input().strip()]) # 입력돤 a의 value 출력
+
+'''
+TIL/회고
+- 바로 딕셔너로 생각이 났던 문제다.
+- 다른 풀이도 크게 다르지는 않다.
+'''
