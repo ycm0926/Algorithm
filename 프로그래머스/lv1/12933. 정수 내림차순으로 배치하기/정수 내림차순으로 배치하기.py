0628daefbd6e46d16b3ad6f8916542866e39bafb
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-28 18:16:18 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,4 +1,17 @@
+# 나의 풀이
+
 def solution(n):
     answer = list(str(n))
     answer.sort(reverse=True)
     return int(''.join(answer))
+
+'''
+# 다른사람 풀이
+
+    return int("".join(sorted(list(str(n)), reverse=True)));
+
+TIL/회고
+- sort는 리턴이 없어 바로 print나 return시none이 출력된다.
+- return에 int를 넣으면 될까 했는데 이렇게도 된다.
+- 다른 사람 풀이도 비슷하다. sorted로 바로 반환해 준다.
+'''
