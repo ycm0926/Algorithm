42532239c1fb9e725493c746f5ea85145f8016e4
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-11-29 05:32:26 +0900

    [Silver III] Title: 피보나치 함수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,24 +1,22 @@
-# d0 = 1, 0
-# d1 = 0, 1
-# d2 = 1, 1
-# d3 = 1, 2
-# d4 = 2. 3
-# d5 = 3, 5
-# d6 = 5, 8
+# 0 -- 1 0
+# 1 -- 0 1
+# 2 -- 1 1
+# 3 -- 1 2
+# 4 -- 2 3
+# 5 -- 3 5
+# 6 -- 5 8
 import sys
 
 input = sys.stdin.readline
-t = int(input())
 
-for i in range(t):
+for _ in range(int(input())):
     n = int(input())
-    d = [] * n
-    d.append([1, 0])
-    d.append([0, 1])
-    d.append([1, 1])
-    if n >= 3:
-        for i in range(3,n+1):
-            d.append([d[i-1][0] + d[i-2][0], d[i-1][1] + d[i-2][1]])
-        print(*d[n])
+    dp = [] * n
+    dp.append([1, 0])
+    dp.append([0, 1])
+    if n < 2:
+        print(*dp[n])
     else:
-        print(*d[n])
+        for j in range(2,n+1):
+            dp.append([dp[j-1][0]+dp[j-2][0],dp[j-1][1]+dp[j-2][1]])
+        print(*dp[n])
