43f58c78cf5cf641eeb371ab576b01fd4a13bae5
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-12-06 03:21:56 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 288 ms, Memory: 40596 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,13 +1,12 @@
 import sys
-if __name__ == '__main__':
-    N, M = map(int, input().split())
-    numbers = list(map(int,input().split()))
-    for i in range(1, N):
-        numbers[i] = numbers[i] + numbers[i-1]
 
-    for _ in range(M):
-        start, end = map(int, sys.stdin.readline().split())
-        if start == 1:
-            print(numbers[end-1])
-        else:
-            print(numbers[end-1]-numbers[start-2])
+N, M = map(int,input().split())
+input = sys.stdin.readline
+lis = [0]       # 1번째 부터 값을 받기 위함
+for i in map(int,input().split()):
+    lis.append(i)
+for i in range(1,N+1):      # 합을 구하는 반복문
+    lis[i] = lis[i] + lis[i-1]
+for _ in range(M):      # 누적 합
+    a,b = map(int, input().split())
+    print(lis[b]-lis[a-1])
