0f1e250fe56b144a006d6d4fb6299d9b0165da2b
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-12-23 23:56:08 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,38 @@
-def dfs(n):
-    visited[n] = True
-    print(n, end=' ')
-    for i in graph[n]:
-        if not visited[i]:
-            dfs(i)
-            
-def bfs(n):
-    visited[n] = True
-    queue = deque([n])
-    while queue:
-        v = queue.popleft()
-        print(v, end= ' ')
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-        
 import sys
 from collections import deque
+input = sys.stdin.readline
+N, M, V = map(int,input().split())
 
-n, m, v= map(int,input().split())
-graph = [[] for _ in range(n+1)]
-visited = [False]*(n+1)
+graph = [[]*(N+1) for i in range(N+1)]  # 그래프 생성
 
-for _ in range(m):
-    a, b = map(int,input().split())
-    graph[a].append(b)
+v = [False] * (N+1)                     # 방문 체크를 위한 리스트
+
+for i in range(M):                      # 그래프에 정점들을 넣어줌
+    a,b = map(int,input().split())
+    graph[a].append(b)                  # 양방향이므로 양쪽을 넣어준다.
     graph[b].append(a)
-    
-for i in range(1, n+1):
-    graph[i].sort()
-    
-dfs(v)
 
-visited = [False]*(n+1)
+for i in range(1, N+1):                 # 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문
+    graph[i].sort()
+def dfs(n):                             # dfs 탐색
+    v[n] = True
+    print(n, end=' ')
+    for i in graph[n]:
+        if not v[i]:
+            dfs(i)
+
+dfs(V)
 print()
-bfs(v)
+v = [False] * (N + 1)                   # bfs를 위한 새로운 방문 리스트
+
+def bfs(n):                             # bfs 탐색
+    q = deque([n])
+    v[n] = True
+    while q:
+        n = q.popleft()
+        print(n, end=' ')
+        for i in graph[n]:
+            if not v[i]:
+                v[i] = True
+                q.append(i)
+bfs(V)
