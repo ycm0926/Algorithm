b3106d5476c59d950c330afd90dc3e53cd852624
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-12-27 14:43:22 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,13 +1,7 @@
-N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-A.sort()
-B.sort(reverse=True)
+n = int(input())
+A, B = list(map(int,input().split())), list(map(int,input().split()))
+a, b = sorted(A), sorted(B, reverse=True)
 res = 0
-for i in range(N):
-    res += A[i]*B[i]
+for i in range(len(a)):
+    res += a[i]*b[i]
 print(res)
-'''
-TIL/회고
-- 문제를 최대한 분석해서 쉽게 풀어보자.
-'''
