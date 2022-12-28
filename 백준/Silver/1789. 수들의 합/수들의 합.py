d6457c4173b445271c3cd06835f7cfb84dd39524
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2022-12-29 03:02:13 +0900

    [Silver V] Title: 수들의 합, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,21 +1,10 @@
-'''
-11
-1 1 i=1
-3 1,2 i=2
-6 1,2,3 i=3
-10 1,2,3,4 i=4
-15 1,2,3,4,5 i=5
-10 1,2,3,4 i=5
-6 1,2,3 i=5
-11 1,2,3,4,5 i=5
-'''
 S = int(input())
-total = 0
-i = 0
-
-while 1:
-    i += 1
-    total += i
-    if total + i+1 > S:
-        print(i)
-        break
+res = 0
+if S == 1:
+    print(1)
+else:
+    for i in range(1,S+1):
+        res += i
+        if res > S:
+            print(i-1)
+            break
