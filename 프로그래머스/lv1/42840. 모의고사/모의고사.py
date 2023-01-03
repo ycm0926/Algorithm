0d44bc31ed4410b3d5223b2b27048d1233bb4444
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-04 01:05:37 +0900

    [level 1] Title: 모의고사, Time: 5.51 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,35 +1,36 @@
 def solution(answers):
-    first = [5 if i%5==0 else i%5 for i in range(1,len(answers)+1)]
+    first = [5 if i%5==0 else i%5 for i in range(1,len(answers)+1)] # first 리스트 만들기
     second = []
     third = []
 
     cnt = 1
-    for i in range(len(answers)):
-        if i % 2 == 0:
+    for i in range(len(answers)):                                   # second 리스트 만들기
+        if i % 2 == 0:                                              # index가 짝수면 2를 추가
             second.append(2)
-        else:
+        else:                                                       # index가 홀수면 cnt를 추가
             second.append(cnt)
-            cnt += 1
-            if cnt == 2:
+            cnt += 1                                                # cnt에 1 더해줌
+            if cnt == 2:                                            # cnt가 2면 1더해 3을 만듦
                 cnt += 1
-            if cnt == 6:
+            if cnt == 6:                                            # cnt가 6이면 1로 초기화
                 cnt = 1
 
     cnt = 3
-    for i in range(len(answers)):                                 
-        third.append(cnt)                                   
-        if len(third) % 2 == 0:                             
-            if cnt == 3 :                                   
-                cnt = 1                                    
-            elif cnt == 5:                                  
-                cnt = 3                                     
-            else:                                          
-                cnt += 1                                   
+    for i in range(len(answers)):                                   # third 리스트 만들기
+        third.append(cnt)                                           # third에 cnt 추가
+        if len(third) % 2 == 0:                                     # 길이가 짝수고
+            if cnt == 3:                                            # cnt가 3이면 1로 초기화
+                cnt = 1
+            elif cnt == 5:                                          # cnt가 5면 3으로 초기화
+                cnt = 3
+            else:                                                   # cnt가 3,5아니라면 cnt += 1
+                cnt += 1                                            # 만약 third[-1] == 2라면 cnt 4로 초기화
                 if third[-1] == 2:
                     cnt = 4
+
     lis = [0,0,0]
     ans = []
-    for i in range(len(answers)):
+    for i in range(len(answers)):                                   # 수포자 각자 몇개 정답인지 카운트
         if answers[i] == first[i]:
             lis[0] +=1
         if answers[i] == second[i]:
@@ -37,7 +38,11 @@ def solution(answers):
         if answers[i] == third[i]:
             lis[2] +=1
 
-    for i in range(len(lis)):
+    for i in range(len(lis)):                                       # 최대 값과 같은 값의 index를 ans에 추가
         if max(lis) == lis[i]:
             ans.append(i+1)
     return ans
+
+
+# 무식해 보여도 사실은 최고의 방법일 때가 있지요.
+# 가능한 모든 상황을 조사해 문제를 풀어보세요.
