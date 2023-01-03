bf19f7272a906d0a2ff9bb4e9b2c8a2983723e45
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-03 18:33:05 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 92 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -1,9 +1,8 @@
-import math
 n = int(input())
 dp = [0] * 10001
 for i in range(1,n+1):
     cnt = 0
-    for j in range(1, int(math.sqrt(i))+1):
+    for j in range(1, int(i**0.5)+1):
         if i % j == 0:
             cnt += 1
     dp[i] = dp[i-1] + cnt
