85d9038b4ce87df7d67329fb464b6e30eb3d5cdb
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-03 21:46:13 +0900

    [level 1] Title: 소수 찾기, Time: 307.47 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,11 +1,7 @@
 def solution(n):
-    a = [False,False] + [True]*(n-1)
-    primes= 0
+    num=set(range(2,n+1))
 
     for i in range(2,n+1):
-        if a[i]:
-            primes += 1
-            for j in range(2*i, n+1, i):
-                a[j] = False
-        
-    return primes
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
