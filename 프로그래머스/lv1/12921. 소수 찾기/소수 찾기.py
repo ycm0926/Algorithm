68cde3471fdb4c2c762ad32f7aef63780dfc6332
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-03 21:34:12 +0900

    [level 1] Title: 소수 찾기, Time: 3118.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,13 +1,11 @@
 def solution(n):
-    if n == 1:
-        return n
     res = 0
-    for i in range(2,n+1):
-        cnt = False
-        for j in range(2,int(i**0.5)+1):
+    for i in range(2,n+1):                      # 2부터 n까지 소수 찾기
+        cnt = False                             # 소수 체크
+        for j in range(2,int(i**0.5)+1):        # 제곱근 까지 순회
             if i % j == 0:
-                cnt = True
+                cnt = True                      # 소수가 아니라면 True
                 break
-        if cnt == False:
+        if cnt == False:                        # False이면 소수이므로 res + 1
             res += 1
     return res
