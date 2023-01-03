32326b65ab7c29ccc207294d57d43329a6655610
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-03 21:45:44 +0900

    [level 1] Title: 소수 찾기, Time: 159.02 ms, Memory: 24 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/README.md
+++ 프로그래머스/lv1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 3118.72 ms
+메모리: 24 MB, 시간: 159.02 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,11 +1,11 @@
 def solution(n):
-    res = 0
-    for i in range(2,n+1):                      # 2부터 n까지 소수 찾기
-        cnt = False                             # 소수 체크
-        for j in range(2,int(i**0.5)+1):        # 제곱근 까지 순회
-            if i % j == 0:
-                cnt = True                      # 소수가 아니라면 True
-                break
-        if cnt == False:                        # False이면 소수이므로 res + 1
-            res += 1
-    return res
+    a = [False,False] + [True]*(n-1)
+    primes= 0
+
+    for i in range(2,n+1):
+        if a[i]:
+            primes += 1
+            for j in range(2*i, n+1, i):
+                a[j] = False
+        
+    return primes
