f36ac22a1460e7e73ba49593164ca0546ce57d1d
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-05 03:18:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,5 +1,2 @@
-import sys
 from itertools import combinations
-
-d = [int(input()) for _ in range(9)]
-print(*[sorted(i) for i in combinations(d,7) if sum(i) == 100][0],sep='\n')
+print(*[sorted(i) for i in combinations([int(input()) for _ in range(9)],7) if sum(i) == 100][0],sep='\n')
