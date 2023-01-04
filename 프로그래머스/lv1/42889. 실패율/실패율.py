833720cc2764822dd80df91d23858887781c5b10
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-04 22:36:20 +0900

    [level 1] Title: 실패율, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,13 +1,16 @@
-from collections import Counter
-
 def solution(N, stages):
-    user = len(stages)              # 사용자 수
-    answer = [0] * (N+1)            # 실패율 리스트
-    for i in range(1,N+1):
-        k = stages.count(i)         # 스테이지 i번에서 실패한 사람 카운트
-        if k == 0:                  # 모두가 통과 했다면 순번과 100을 넣어준다.
-            answer[i] = (i,0)
-            continue
-        answer[i] = (i,k/user)      # 실패한 사람이 있다면 순번과 실패율을 answer에 넣어준다.
-        user -= k                   # 전체 유저수에서 실패한 유저수를 빼준다.
-    return [i[0] for i in sorted(answer[1:], key =  lambda x: -x[1])]
+    answer = []
+    fail = []
+    info = [0] * (N + 2)
+    for stage in stages:
+        info[stage] += 1
+    for i in range(N):
+        be = sum(info[(i + 1):])
+        yet = info[i + 1]
+        if be == 0:
+            fail.append((str(i + 1), 0))
+        else:
+            fail.append((str(i + 1), yet / be))
+    for item in sorted(fail, key=lambda x: x[1], reverse=True):
+        answer.append(int(item[0]))
+    return answer
