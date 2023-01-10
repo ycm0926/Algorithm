41e2576cb7d842d2b43801a64a8dd830f63ef048
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-10 14:20:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,21 +1,21 @@
 import sys
 
 input = sys.stdin.readline
-k, n = map(int, input().split())
 
-k_list = [int(input()) for _ in range(k)]
-start, end = 1, max(k_list)
-
-while (start <= end):
+K, M = map(int,input().split())
+LAN = [int(input()) for i in range(K)]
+start = 1
+end = max(LAN)
+while start <= end:
     total = 0
-    mid = (start+end)//2
-    for x in k_list:
-        total += x//mid
-    # 전선이 부족한 경우 왼쪽 탐색
-    if total < n:
-        end = mid-1
-    # 오른쪽 탐색
-    else:
-        start = mid+1
+    mid = (start+end)//2                # 자를 랜선 길이 지정
 
+    for i in LAN:
+        total += i//mid                 # 자른 랜선 수
+
+    if total < M:                       # 랜선의 개수가 부족한 경우 더 짧게 자르기(왼쪽 탐색)
+        end = mid - 1
+
+    else:                               # 랜선의 개수가 충분한 경우 덜 자르기(오른쪽 탐색)
+        start = mid + 1
 print(end)
