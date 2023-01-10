8bbcadd6528849cbf9fbc99198df137439b18c64
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-10 23:09:32 +0900

    [Silver II] Title: 나무 자르기, Time: 4768 ms, Memory: 148896 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,24 +1,24 @@
-n, m = map(int, input().split())
-n_list = list(map(int, input().split()))
+import sys
 
-start = 0
-end = max(n_list)
+input = sys.stdin.readline
 
-result = 0
-while (start <= end):
+N, M = map(int,input().split())
+h = list(map(int,input().split()))
+
+start = 1
+end = max(h)
+
+while start <= end:
     total = 0
     mid = (start+end)//2
-    for n in n_list:
-        if n > mid:
-            total += n - mid
-    # total 이 작으므로 왼쪽으로 탐색
-    if total < m:
+
+    for i in h:
+        if i > mid:
+            total += i-mid
+
+    if total < M:
         end = mid - 1
-    # 오른쪽으로 탐색
     else:
-        result = mid
         start = mid + 1
 
-print(result)
-
-
+print(end)
