61646c9390cdf3f7d63e781abb9cf2290ab616b1
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-12 21:09:54 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,7 +1,14 @@
-# 나의 풀이
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-dp = [0,1]
+dp = [0] * (n+1)
+dp[1] = 1
+
+if n <= 1:
+    print(dp[n])
+    exit(0)
+
 for i in range(2,n+1):
-    dp.append(dp[i-1]+dp[i-2])
+    dp[i] = dp[i-1]+dp[i-2]
 print(dp[n])
