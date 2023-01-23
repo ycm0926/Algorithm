c1ab80e903a11652f55cb8e1fdc8d2aff2ccc1d2
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-23 18:52:27 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 316 ms, Memory: 34336 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.py
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.py
@@ -3,17 +3,17 @@ import sys
 input = sys.stdin.readline
 N, K = map(int,input().split())
 characters = [int(input()) for i in range(N)]
-start = min(characters)
-end =  1000000001
+start = min(characters)                         # start값은 가장 작은값부터
+end =  start+K                                  # end는 가장 작은 값에서 K를 더한 값
 while start <= end:
     mid = (start+end)//2
     cnt = 0
 
-    for i in characters:
+    for i in characters:                        # 레벨이 mid보다 작다면 차이 만큼 카운트
         if i < mid:
             cnt += mid-i
 
-    if cnt <= K:
+    if cnt <= K:                                # 카운트 한 값이 K보다 작거나 같다면 start 이동
         start = mid + 1
     else:
         end = mid - 1
