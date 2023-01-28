2e0a372aa6070c48bd0dd2c40224cc8516e81c68
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-29 02:25:46 +0900

    [Silver IV] Title: 상근이의 여행, Time: 172 ms, Memory: 31660 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,22 +1,17 @@
 import sys
-input = sys.stdin.readline
 
+
+input = sys.stdin.readline
 tc = int(input())
-def dfs(n):
-    global cnt
-    visited[n] = True
-    cnt += 1
-    for i in graph[n]:
-        if not visited[i]:
-            dfs(i)
+
 for i in range(tc):
-    n, m = map(int, input().split())
-    graph = [[]*(m+1) for i in range(n+1)]
-    visited = [False] * (n+1)
+    N, M = map(int,input().split())
+    graph = [[] * (N+1) for i in range(N+1)]
+    visited = [False] * (N+1)
     cnt = 0
-    for i in range(m):
+    for i in range(M):
         a, b = map(int,input().split())
         graph[a].append(b)
         graph[b].append(a)
-    dfs(1)
-    print(cnt-1)
+
+    print(N-1)
