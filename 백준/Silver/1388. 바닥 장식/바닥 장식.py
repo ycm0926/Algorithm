97f5e9e39c6c0f5b5026e07125c591533fded0c6
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-29 03:23:24 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,31 +1,38 @@
-N, M = map(int, input().split())
-graph = [list(input()) for i in range(N)]
-check_c = 0
-check_r = 0
-cnt = 0
+import sys
 
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+graph = [input() for i in range(N)]
+total = 0
 for i in range(N):
-    for j in range(M):
-        if graph[i][j] == '-':
-            check_c += 1
-        elif graph[i][j] == '|':
-            if check_c >= 1:
-                cnt +=1
-            check_c = 0
-    if check_c >= 1:
-        cnt +=1
-    check_c = 0
-
+    check = False
+    cnt = 0
+    for j in graph[i]:
+        if j == '-' and check:
+            continue
+        elif j == '-':
+            cnt += 1
+            check = True
+            continue
+        elif j == '|':
+            total += cnt
+            cnt = 0
+            check = False
+    total += cnt
 for i in range(M):
+    check = False
+    cnt = 0
     for j in range(N):
-        if graph[j][i] == '|':
-            check_r += 1
+        if graph[j][i] == '|' and check:
+            continue
+        elif graph[j][i] == '|':
+            cnt += 1
+            check = True
+            continue
         elif graph[j][i] == '-':
-            if check_r >= 1:
-                cnt +=1
-            check_r = 0
-    if check_r >= 1:
-        cnt +=1
-    check_r = 0
-
-print(cnt)
+            total += cnt
+            cnt = 0
+            check = False
+    total += cnt
+print(total)
