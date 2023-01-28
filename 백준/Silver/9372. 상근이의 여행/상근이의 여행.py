8e8c2851fd3d2e14a4c44665fb68384cdbcc9509
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-29 02:27:21 +0900

    [Silver IV] Title: 상근이의 여행, Time: 60 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,17 +1,10 @@
 import sys
 
-
 input = sys.stdin.readline
-tc = int(input())
 
-for i in range(tc):
+for i in range(int(input())):
     N, M = map(int,input().split())
-    graph = [[] * (N+1) for i in range(N+1)]
-    visited = [False] * (N+1)
-    cnt = 0
     for i in range(M):
-        a, b = map(int,input().split())
-        graph[a].append(b)
-        graph[b].append(a)
+        input()
 
     print(N-1)
