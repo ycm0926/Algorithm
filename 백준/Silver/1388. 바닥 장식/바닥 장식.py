e8b3d02fade0c3b9edd815f4f27efe374cf1d9d9
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-01-29 03:56:45 +0900

    [Silver IV] Title: 바닥 장식, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,38 +1,27 @@
 import sys
 
 input = sys.stdin.readline
+def dfs(x,y):
+    global cnt
+
+    if x < 0 or x >= N or y < 0 or y >= M:
+        return False
+
+    visited[x][y] = True
+
+    if y+1 < M and graph[x][y] == '-' and graph[x][y+1] == '-':     # `-`이고 다음이 `-`일 경우 재귀
+        dfs(x,y+1)
+    elif x+1 < N and graph[x][y] == '|' and graph[x+1][y] == '|':   # `|`이고 다음이 `|`일 경우 재귀
+        dfs(x+1,y)
 
 N, M = map(int,input().split())
 graph = [input() for i in range(N)]
+visited = [[False]*M for i in range(N)]
 total = 0
+
 for i in range(N):
-    check = False
-    cnt = 0
-    for j in graph[i]:
-        if j == '-' and check:
-            continue
-        elif j == '-':
-            cnt += 1
-            check = True
-            continue
-        elif j == '|':
-            total += cnt
-            cnt = 0
-            check = False
-    total += cnt
-for i in range(M):
-    check = False
-    cnt = 0
-    for j in range(N):
-        if graph[j][i] == '|' and check:
-            continue
-        elif graph[j][i] == '|':
-            cnt += 1
-            check = True
-            continue
-        elif graph[j][i] == '-':
-            total += cnt
-            cnt = 0
-            check = False
-    total += cnt
+    for j in range(M):
+        if visited[i][j] == False:
+            dfs(i,j)
+            total += 1
 print(total)
