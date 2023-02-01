60a3c267f39515eba742e06c489b206ec1473116
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-02 02:01:00 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -3,7 +3,6 @@ def solution(survey, choices):
 
     li = {1:3, 2:2, 3:1, 4:0, 5:1, 6:2, 7:3}
     cnt = {'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0}
-    mbti_list = ['R','T','C','F','J','M','A','N']
 
     for s,c in zip(survey,choices):
         if c == 4:
@@ -22,14 +21,10 @@ def solution(survey, choices):
         answer += 'C'
     else:
         answer += 'F'
-
-    
     if cnt['J'] >= cnt['M']:
         answer += 'J'
     else:
         answer += 'M'
-
-    
     if cnt['A'] >= cnt['N']:
         answer += 'A'
     else:
