a32b67517f5dc4a86338a39301b9285e3c360acd
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-03 05:21:25 +0900

    [unrated] Title: 둘만의 암호, Time: 0.19 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,3 +1,16 @@
 def solution(s, skip, index):
-    answer = ''
+    answer = ""
+    skip_ord = [i-97 for i in list(map(ord, skip))]     # skip 값 변환
+    for i in s:                                         # 문자열 s에서 하나씩 가져온다.
+        i_o = ord(i)-97                                 # i_o의 범위는 0~25  
+        cnt = 0                                         # 이동 카운트 변수
+        while cnt != index:                             # cnt가 index와 같을 때까지 반복
+            i_o = i_o+1                                 # i_o를 i_o+1로 초기화 -> 알파벳 하나씩 이동
+            if i_o >= 26:                               # i_o가 26을 넘는다면 %연산으로 0~25범위로
+                i_o %= 26
+            if i_o not in skip_ord:                     # i_o가 skip에 없다면 cnt 증가
+                cnt += 1
+
+        answer += chr(i_o+97)                           # 알파벳으로 변환
+
     return answer
