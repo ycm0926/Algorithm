ca843cf8cbcd6eaaac4471f2ef4870263d7957b4
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-03 05:46:35 +0900

    [unrated] Title: 둘만의 암호, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.c
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.c
@@ -1,17 +1,10 @@
-def solution(s, skip, index):
-    answer = ""
-    skip_ord = [i-97 for i in list(map(ord, skip))]     # skip 값 변환
-    for i in s:                                         # 문자열 s에서 하나씩 가져온다.
-        i_o = ord(i)-97                                 # i_o의 범위는 0~25  
-        cnt = 0                                         # 이동 카운트 변수
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-        while cnt != index:                             # cnt가 index와 같을 때까지 반복
-            i_o = i_o+1                                 # i_o를 i_o+1로 초기화 -> 알파벳 하나씩 이동
-            if i_o >= 26:                               # i_o가 26을 넘는다면 %연산으로 0~25범위로
-                i_o %= 26
-            if i_o not in skip_ord:                     # i_o가 skip에 없다면 cnt 증가
-                cnt += 1
-
-        answer += chr(i_o+97)                           # 알파벳으로 변환
-
-    return answer
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* s, const char* skip, int index) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(1);
+    return answer;
+}
