9f63e159452f073a42e635f70b3a606532d5f26d
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-06 01:22:20 +0900

    [Silver I] Title: 경로 찾기, Time: 228 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -5,28 +5,13 @@ INF = int(1e9)
 N = int(input())
 graph = [list(map(int,input().split())) for i in range(N)]
 
-# 자기자신 초기화
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 0:
-            graph[i][j] = INF
-
 # 점화식
 for k in range(N):
     for a in range(N):
         for b in range(N):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+            if graph[a][k] and graph[k][b]:
+                graph[a][b] = 1
 
-# 정답 그래프 생성
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == INF:
-            graph[i][j] = 0
-        else:
-            graph[i][j] = 1
-
-# 정답 그래프 출력
+# 정답 출력
 for i in graph:
-    for j in i:
-        print(j, end=' ')
-    print()
+    print(*i)
