711bf733d303ed22af04d12c9a32394eb39e1657
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-06 00:31:28 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,28 +1,35 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-INF = int(1e9)                                                          # 무한을 의미하는 값으로 10억을 설정
+
+INF = int(1e9)
 N, M = map(int,input().split())
 graph = [[INF]*(N+1) for i in range(N+1)]
+res = []
 
-for a in range(1, N+1):                                                 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+# 자기자신 0 초기화
+for a in range(1,N+1):
     for b in range(1, N+1):
-        if a == b:
+        if a==b:
             graph[a][b] = 0
 
-for _ in range(M):                                                      # A에서 B로 가는 비용은 1로 설정
-    a, b = map(int, input().split())
+# 간선 초기화
+for _ in range(M):
+    a, b = map(int,input().split())
     graph[a][b] = 1
     graph[b][a] = 1
 
-for k in range(1, N+1):                                                 # 점화식에 따라 플로이드 워셜 알고리즘을 수행
-    for a in range(1, N+1):
-        for b in range(1, N+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+# 점화식
+for k in range(1,N+1):
+    for a in range(1,N+1):
+        for b in range(1,N+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
-res = min(map(sum,graph))                                               # graph에서 가장 작은 값 초기화
+# 케빈 베이컨의 수가 가장 작은 사람 출력
+for i in range(1,N+1):
+    cnt = 0
+    for j in range(1,N+1):
+        if graph[i][j] != INF and graph[i][j] != 0:
+            cnt += graph[i][j]
+    res.append(cnt)
 
-for i in range(N+1):                                                    # graph에서 가장 작은 값 위치 출력
-    if res == sum(graph[i]):
-        print(i)
-        break
+print(res.index(min(res))+1)
