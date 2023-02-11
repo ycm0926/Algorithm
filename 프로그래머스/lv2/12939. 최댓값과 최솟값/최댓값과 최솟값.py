886340952d048f03b8dd33cf5f9df52aa66ba8b6
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-11 18:18:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,5 +1,3 @@
 def solution(s):
-    answer = ''
-    a = sorted(list(map(int,s.split())))
-    answer += str(a[0])+' '+str(a[-1])
-    return answer
+    s = list(map(int,s.split()))
+    return str(min(s)) + " " + str(max(s))
