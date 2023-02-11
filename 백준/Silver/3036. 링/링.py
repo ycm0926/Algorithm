3cd4aa1344162d5f44301082df0fd66ff73df70d
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-12 02:25:13 +0900

    [Silver IV] Title: 링, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/3036. 링/링.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+_= input()
+ring = list(map(int,input().split()))
+b_ring = ring[0]
+
+def gcd(a,b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+for i in ring[1:]:
+    num = gcd(b_ring,i)
+    print(f'{b_ring//num}/{i//num}')
