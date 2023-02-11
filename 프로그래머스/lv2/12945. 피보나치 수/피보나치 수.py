4b6fb5a280040f4ec55ca3e8c69ddf8c4f4050ab
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-12 01:26:27 +0900

    [level 2] Title: 피보나치 수, Time: 6.38 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,6 @@
-def solution(n):
-    dp = [0]*(n+1)
-    dp[1] = 1
+def solution(num):
+    a,b = 0,1
+    for i in range(num):
+        a,b = b,(a+b)%1234567
+    return a
 
-    for i in range(2,n+1):
-        dp[i] = dp[i-1]+dp[i-2]
-
-    return dp[-1]%1234567
