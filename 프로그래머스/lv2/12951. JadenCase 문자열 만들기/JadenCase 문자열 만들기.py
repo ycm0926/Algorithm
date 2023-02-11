14a7214fe9153cedeff593d501bc3b2ce7a52a21
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-11 19:49:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,7 +1,2 @@
 def solution(s):
-    jc = s.split(' ')
-
-    for i in range(len(jc)):
-        jc[i] = jc[i].capitalize()
-
-    return ' '.join(jc)
+    return " ".join([x.capitalize() for x in s.lower().split(' ')])   
