8e477bbdcf76621c50fb3c7d6ab4c44811adc15a
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-11 23:52:07 +0900

    [level 2] Title: 숫자의 표현, Time: 3.65 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 0
+    Finn = [i for i in range(1,(n//2)+2)]
+    l_Finn = len(Finn)
+    
+    for i in range(l_Finn):
+        cnt = Finn[i]
+        for j in range(i+1, l_Finn):
+            cnt += Finn[j]
+            if cnt == n:
+                answer += 1
+                break
+            if cnt >= n:
+                break
+
+    return answer+1
+                
