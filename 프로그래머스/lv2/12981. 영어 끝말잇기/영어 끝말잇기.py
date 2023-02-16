e7c905606f379a6751737ba4ea47ca94b48cdb91
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-16 19:29:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,7 +1,17 @@
+
 def solution(n, words):
-    answer = []
+    s_words = set([words[0]])
+    num = 1
+    order = 1
+    for i in range(1,len(words)):
+        num += 1
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+        if  num > n:
+            num = 1
+            order += 1
 
-    return answer
+        if words[i] in s_words or words[i-1][-1] != words[i][0]:
+            return [num,order]
+
+        s_words.add(words[i])
+    return [0,0]
