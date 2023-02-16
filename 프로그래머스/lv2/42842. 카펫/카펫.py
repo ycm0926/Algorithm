75ed64eeafa3c20f4762f5f29269fb6c283baeae
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-16 22:07:58 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,11 @@
+
+def solution(brown, yellow):
+    t = brown + yellow
+
+    for col in range(3,brown):                          # 행의 최소 개수는 3부터
+        row = t//col
+        if t%col == 0 and (row-2)*(col-2) == yellow:    # t에서 col의 모듈연산 나머지가 0이고 노란색 타일의 개수가 yellow 일 때
+            if row > col:
+                row, col = col, row
+            
+            return [col, row]
