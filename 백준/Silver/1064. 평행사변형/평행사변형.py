33a50b423a4d6df7321d8a4bd7ab48cf11962662
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-20 19:18:15 +0900

    [Silver V] Title: 평행사변형, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1064. 평행사변형/평행사변형.py
+++ 백준/Silver/1064. 평행사변형/평행사변형.py
@@ -4,13 +4,15 @@ input = sys.stdin.readline
 
 xa, ya ,xb, yb, xc, yc = map(int,input().split())
 
-
+# y축에 평행한 경우
 if (xa-xb) == 0 and (xc-xb) == 0:
     print(-1.0)
+
+# 한 직선에 있는 경우
 elif (xa-xb) != 0 and (xc-xb) != 0 and (ya-yb)/(xa-xb) == (yc-yb)/(xc-xb): 
     print(-1.0)
 else:
-    a = ((((xa-xb)**2+(ya-yb)**2)**0.5)*2)+((((xa-xc)**2+(ya-yc)**2)**0.5)*2)
-    b = ((((xb-xc)**2+(yb-yc)**2)**0.5)*2)+((((xb-xa)**2+(yb-ya)**2)**0.5)*2)
-    c = ((((xc-xa)**2+(yc-ya)**2)**0.5)*2)+((((xc-xb)**2+(yc-yb)**2)**0.5)*2)
+    a = (((((xa-xb)**2+(ya-yb)**2)**0.5))+((((xa-xc)**2+(ya-yc)**2)**0.5)))*2
+    b = (((((xb-xc)**2+(yb-yc)**2)**0.5))+((((xb-xa)**2+(yb-ya)**2)**0.5)))*2
+    c = (((((xc-xa)**2+(yc-ya)**2)**0.5))+((((xc-xb)**2+(yc-yb)**2)**0.5)))*2
     print(max(a,b,c)-min(a,b,c))
