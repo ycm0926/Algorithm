39ba21de9ce443d75cc2fffa71728be70e62c5fd
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-20 19:40:35 +0900

    [Bronze I] Title: 택시 거리, Time: 196 ms, Memory: 36572 KB -BaekjoonHub

---
+++ 백준/Bronze/17247. 택시 거리/택시 거리.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+
+arr = []
+a = []
+for i in range(N):
+    arr.append(list(map(int,input().split())))
+
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 1:
+            if a:
+                print(abs(i-a[0])+abs(j-a[1]))
+                exit()
+            a = [i,j]
