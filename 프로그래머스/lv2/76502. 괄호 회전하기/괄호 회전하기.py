9ab1d129ccdfa1f102f0b2a34cf8beb9cd17f4d1
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-23 19:59:35 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,25 +1,22 @@
-from collections import deque
+def is_valid(s):
+    stack = []
+    for ch in s:
+        if not stack:
+            stack.append(ch)
+        elif stack[-1] == '(':
+            if ch==')': stack.pop()
+            else: stack.append(ch)
+        elif stack[-1] == '{':
+            if ch=='}': stack.pop()
+            else: stack.append(ch)
+        elif stack[-1] == '[':
+            if ch==']': stack.pop()
+            else: stack.append(ch)
+
+    return False if stack else True
 
 def solution(s):
     answer = 0
-    
-    if len(s)%2 != 0:
-        return 0
-    
-    for i in range(len(s)-1):
-        stack = [s[0]]
-        
-        for i in s[1:]:
-            
-            if not stack:
-                stack.append(i)            
-            elif (stack[-1] == '[' and i == ']') or (stack[-1] == '(' and i == ')') or (stack[-1] == '{' and i == '}'):
-                stack.pop()
-            else:
-                stack.append(i)
-                
-        if not stack:
-            answer +=1
-            
-        s = s[1:]+s[0]
+    for i in range(len(s)):
+        answer += is_valid(s[i:]+s[:i])
     return answer
