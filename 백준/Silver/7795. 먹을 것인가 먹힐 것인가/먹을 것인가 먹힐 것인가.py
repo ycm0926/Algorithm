f3c8afe87cc8c12253ab5dad16bd464d15c93000
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-02-28 18:29:11 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 148 ms, Memory: 34684 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,22 +1,18 @@
 import sys
 
 input = sys.stdin.readline
-T = int(input())
-for i in range(T):
+for i in range(int(input())):
     cnt = 0
     N, M = map(int,input().split())
-    A = list(map(int,input().split()))
+    A = sorted(map(int,input().split()))
     B = sorted(map(int,input().split()))
+    idx, cnt = 0, 0
 
-    for i in A:
-        s, e = 0, M-1
-        while s <= e:
-            
-            mid = (s+e)//2
-    
-            if i > B[mid]:
-                s = mid+1
+    for i in range(N):
+        while idx < M:
+            if A[i] > B[idx]:
+                idx += 1
             else:
-                e = mid-1
-        cnt += s
+                break
+        cnt += idx
     print(cnt)
