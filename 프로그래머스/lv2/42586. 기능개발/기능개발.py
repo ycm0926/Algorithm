d108f6966ee9d67ce1b43df7747e7e95ba9cd6c6
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-03-03 15:37:01 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,17 +1,18 @@
-import math
 def solution(progresses, speeds):
+    print(progresses)
+    print(speeds)
     answer = []
-    days = []
-    for p,s in zip(progresses, speeds):
-        days.append(math.ceil((100-p)/s))
-    
-    day = 0
-    
-    for i in range(1,len(days)):
-        if days[i] > days[day]:
-            answer.append(i-day)
-            day = i
-            
-    answer.append(len(days)-day)
-    
+    time = 0
+    count = 0
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
     return answer
