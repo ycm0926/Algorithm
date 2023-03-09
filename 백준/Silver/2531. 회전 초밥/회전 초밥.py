bef093de63be6a66a845cbb83dcec4b0a02dfe57
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-03-09 22:49:32 +0900

    [Silver I] Title: 회전 초밥, Time: 4412 ms, Memory: 33544 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,13 @@
+N, d, k, c = map(int, input().split())
+sushi = [int(input()) for i in range(N)]
+answer = []
+
+for i in range(N):
+    
+    if i+k > N:
+        answer.append(len(set(sushi[i:]+sushi[:(i+k)%N]+[c])))
+    else:
+        answer.append(len(set(sushi[i:i+k]+[c])))
+
+
+print(max(answer))
