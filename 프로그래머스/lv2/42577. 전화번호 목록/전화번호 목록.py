d481ac417daa786897880917ff7618ca722767da
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-03-20 02:20:39 +0900

    [level 2] Title: 전화번호 목록, Time: 86.45 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,7 @@
-def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
             return False
     return True
