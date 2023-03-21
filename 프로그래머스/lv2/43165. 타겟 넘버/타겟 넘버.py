d2c0471646869617a5368e25a328ea69f45836d6
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-03-22 01:36:25 +0900

    [level 2] Title: 타겟 넘버, Time: 1.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,13 @@
 def solution(numbers, target):
-    sup = [0]
-    for i in numbers:
-        sub = []
-        for j in sup : 
-            sub.append(j+i)
-            sub.append(j-i)
-        sup = sub
+    idx = curr = 0
 
-    return sup.count(target)
+    def dfs(nums, target, curr, idx):
+        # 결과 비교는 마지막에 인덱스가 끝까지 갔을 때 합니다
+        if idx == len(nums):
+            return 1 if curr == target else 0
+
+        # idx를 한칸 움직여주고 curr에 이번 값을 더하거나 뺍니다
+        return dfs(nums, target, curr + nums[idx], idx + 1) + dfs(nums, target, curr - nums[idx], idx + 1)
+
+    answer = dfs(numbers, target, idx, curr)
+    return answer
