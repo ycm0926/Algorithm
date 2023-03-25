af493350442e71ebfcbbcb1a2f997c3c77501a63
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-03-26 02:14:28 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,11 +1,10 @@
-from copy import deepcopy as dc
+import copy
 def solution(want, number, discount):
     answer = 0
     tmp = dict(zip(want,number))
-    diff = len(discount)-9
     
-    for i in range(diff):
-        cnt = dc(tmp)
+    for i in range(len(discount)-9):
+        cnt = copy.copy(tmp)
         for j in discount[i:i+10]:
             if j in cnt:
                 cnt[j] -= 1
