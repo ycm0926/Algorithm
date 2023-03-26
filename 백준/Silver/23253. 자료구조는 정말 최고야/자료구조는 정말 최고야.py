c50ea93dbf4148175c07e18d52567a7ef53fea51
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-03-26 19:10:29 +0900

    [Silver V] Title: 자료구조는 정말 최고야, Time: 260 ms, Memory: 53144 KB -BaekjoonHub

---
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
@@ -0,0 +1,14 @@
+import sys
+
+# 책의 개수, 책의 더미 개수
+n, k = map(int, sys.stdin.readline().rstrip().split())
+
+for i in range(k):
+    sys.stdin.readline() # 책 수를 입력받지만 딱히 쓸 곳은 없으므로 그냥 입력만 받고 넘어감
+
+    books = list(map(int, sys.stdin.readline().rstrip().split()))
+    if books != sorted(books, reverse=True):
+        print("No")
+        exit(0)
+
+print("Yes")
