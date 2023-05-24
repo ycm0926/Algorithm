8c7e440f993e2220ad03ca99a4252580c4db6b31
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-05-25 00:21:41 +0900

    [Silver I] Title: IOIOI, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,7 +5,8 @@ tmp = 'IO'*N+'I'
 cnt = 2*N+1
 ans = 0
 
+# KMP 알고리즘
 for i in range(0,M-(2*N+1)+1):
-    if ''.join(graph[i:i+cnt]) == tmp:
+    if graph[i] == 'I' and graph[i+cnt-1] == 'I' and ''.join(graph[i:i+cnt]) == tmp:
        ans += 1
 print(ans)
