661195d2b4d49e00982653adcbe766487e8a9008
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2023-06-17 01:59:14 +0900

    [Gold V] Title: 토마토, Time: 1512 ms, Memory: 52580 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -9,18 +9,18 @@ from collections import deque
     토마토가 모두 익을 때까지 최소 며칠이 걸리는지를 계산해서 출력해야 한다. 
     만약, 저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고,
     토마토가 모두 익지는 못하는 상황이면 -1을 출력해야 한다. 
-    
+
 '''
 
 input = sys.stdin.readline
-M, N, H = map(int,input().split())
+M, N, H = map(int, input().split())
 ans = 1
-L = N*H
-dx = [0,1,0,-1,N,-N]
-dy = [1,0,-1,0,0,0]
-v = [[False]*M for i in range(L)]
+L = N * H
+dx = [0, 1, 0, -1, N, -N]
+dy = [1, 0, -1, 0, 0, 0]
 
-boxes = [list(map(int,input().split())) for i in range(L)]
+boxes = [list(map(int, input().split())) for i in range(L)]
+
 
 def bfs():
     global ans
@@ -32,22 +32,22 @@ def bfs():
             nx = x + dx[k]
             ny = y + dy[k]
 
-            if 0 <= nx < L and 0 <= ny < M and boxes[nx][ny] == 0 and not v[nx][ny]:
+            if 0 <= nx < L and 0 <= ny < M and boxes[nx][ny] == 0:
                 # x좌표의 경계조건을 높이 생각한 위충, 아래층 조건
-                if k == 1 and x%N == N-1:
+                if k == 1 and x % N == N - 1:
                     continue
-                if k == 3 and x%N == 0:
+                if k == 3 and x % N == 0:
                     continue
 
-                v[nx][ny] = True
-                q.append((nx,ny))
+                q.append((nx, ny))
                 boxes[nx][ny] = boxes[x][y] + 1
+
+
 q = deque([])
 for i in range(L):
     for j in range(M):
         if boxes[i][j] == 1:
-            v[i][j] = True
-            q.append((i,j))
+            q.append((i, j))
 bfs()
 for i in range(L):
     ans = max(ans, max(boxes[i]))
@@ -55,4 +55,4 @@ for i in range(L):
         print(-1)
         exit()
 else:
-    print(ans-1)
+    print(ans - 1)
