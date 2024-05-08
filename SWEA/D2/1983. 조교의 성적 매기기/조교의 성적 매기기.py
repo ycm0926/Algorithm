01e9919e18759485aa5ff8cae2c6baf4bed63243
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2024-05-08 21:00:53 +0900

    [D2] Title: 조교의 성적 매기기, Time: 169 ms, Memory: 45,004 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,15 +1,16 @@
+import math
 
 T = int(input())
 for test_case in range(1, T + 1):
     n, k = map(int,input().split())
     students = dict()
-    grade = ["D0","C-","C0","C+","B-","B0","B+","A-","A0","A+"]
+    grade = [0,"D0","C-","C0","C+","B-","B0","B+","A-","A0","A+"]
     for i in range(n):
         a, b, c = map(int, input().split())
         students[i+1] = (a*0.35) + (b*0.45) + (c*0.2)
     students = sorted(students.items(), key=lambda x: x[1])
 
-    for i,s in enumerate(students):
+    for i,s in enumerate(students,1):
         if s[0] == k:
-            print(f"#{test_case} {grade[(i//(n//10))]}")
+            print(f"#{test_case} {grade[math.ceil(i/(n//10))]}")
             break
