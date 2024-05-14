50f1b28a1d63a214b50391b9a66ccd169a17af5d
Changmin Yang <94760980+ycm0926@users.noreply.github.com>
2024-05-14 21:37:31 +0900

    [D2] Title: 숫자를 정렬하자, Time: 97 ms, Memory: 44,668 KB -BaekjoonHub

---
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -0,0 +1,8 @@
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    _ = input()
+    nums = sorted(list(map(int, input().split())))
+    print(f"#{test_case}", end=' ')
+    print(*nums)
